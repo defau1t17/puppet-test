41a8fc57004cf181f006b9d172eff2c42119b9e2
defau1t17 <mihail.stepushin@gmail.com>
2024-01-19 18:44:02 +0300

    test 1.2

--- site/profile/manifests/slave1/static.pp
+++ site/profile/manifests/slave1/static.pp
@@ -1,11 +1,9 @@
 class profile::slave1::static {
     file { '/usr/share/nginx/html' :
-        ensure => file,
         source => 'puppet:///modules/nginx/static.html'
     }
 
    file { '/etc/nginx/conf.d/' :
-        ensure => file,
         source => 'puppet:///modules/nginx/static.conf'
     }
 }
