faa4790327d85031d6fae2ad0dcf9199cdf72cf3
defau1t17 <mihail.stepushin@gmail.com>
2024-01-19 18:18:16 +0300

    fix 1.1

--- site/profile/manifests/slave1/static-content.pp
+++ site/profile/manifests/slave1/static-content.pp
@@ -1,11 +1,11 @@
 class profile::slave1::static-content {
     file { '/usr/share/nginx/html' :
         ensure => file,
-        source => puppet:///modules/files/static.html
+        source => 'puppet:///modules/files/static.html'
     }
 
    file { '/etc/nginx/conf.d/' :
         ensure => file,
-        source => puppet:///modules/files/static.conf
+        source => 'puppet:///modules/files/static.conf'
     }
 }
--- site/profile/manifests/slave2/dynamic-content.pp
+++ site/profile/manifests/slave2/dynamic-content.pp
@@ -1,11 +1,11 @@
 class profile::slave2::dynamic-content {
     file { '/usr/share/nginx/html' :
         ensure => file,
-        source => puppet:///modules/files/dynamic.php
+        source => 'puppet:///modules/files/dynamic.php'
     }
 
    file { '/etc/nginx/conf.d/' : 
         ensure => file,
-        source => puppet:///modules/files/dynamic.conf
+        source => 'puppet:///modules/files/dynamic.conf'
     }
 }
