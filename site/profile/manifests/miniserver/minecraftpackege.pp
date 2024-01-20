dee0d84a1178f6fec03cbd12fd061cbc5c946a82
defau1t17 <mihail.stepushin@gmail.com>
2024-01-20 19:06:11 +0300

    fix 1.5

--- site/profile/manifests/miniserver/minecraftpackege.pp
+++ site/profile/manifests/miniserver/minecraftpackege.pp
@@ -3,7 +3,7 @@ class profile::miniserver::minecraftpackege {
 
     wget::fetch { 'download minecraft server' :
         source => 'https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar'
-         destination        => '/tmp/minecraft/',
+        destination => '/tmp/minecraft/',
         timeout => 0,
         verbose => true
     }
