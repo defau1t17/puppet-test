ab46b2ea3b442ee576eedcb7976df1deb8d5f21f
defau1t17 <mihail.stepushin@gmail.com>
2024-01-21 02:03:16 +0300

    fix 4.1

--- modules/minecraftserver/manifests/init.pp
+++ modules/minecraftserver/manifests/init.pp
@@ -7,15 +7,10 @@ class minecraftserver {
         ensure => 'directory',
     }
 
-    exec { 'download minecraft server' :
-        command => '/usr/bin/wget https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar -O /opt/minecraft/server.jar',
-        creates => '/opt/minecraft/server.jar',
-    }
-
     include wget
     wget::fetch {'test fetch' :
         source      => 'https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar',
-        destination => '/tmp/',
+        destination => '/opt/minecraft/',
         timeout     => 0,
         verbose     => false,
     }
@@ -31,7 +26,7 @@ class minecraftserver {
         enable => true,
     }
 
-    file_line { 'Update Policy':
+    file_line { 'Update Server Policy':
       ensure => present,
       path   => '/opt/minecraft/eula.txt',
       line   => "eula=true",
