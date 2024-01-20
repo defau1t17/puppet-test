cb40b00333e41737b557aa375bc17f237607b262
defau1t17 <mihail.stepushin@gmail.com>
2024-01-20 19:47:29 +0300

    fix 2.1

--- site/profile/manifests/miniserver/minecraftpackege.pp
+++ site/profile/manifests/miniserver/minecraftpackege.pp
@@ -1,15 +1,8 @@
 class profile::miniserver::minecraftpackege {
-    
-    
     remote_file { 'Download minecraft server' :
         source => 'https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar',
         path   => '/opt/minecraft/server.jar,
         ensure => present,
-    }
-
-    file { '/opt/minecraft/server.jar' :
-        ensure => file,
-        mode => '0777',  
     }
     
 }
