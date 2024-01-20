5491d954017438991f81574109bcdfb7f4c942e8
defau1t17 <mihail.stepushin@gmail.com>
2024-01-21 00:22:37 +0300

    fix 1.5

--- site/profile/manifests/mainserver/reverseproxy.pp
+++ site/profile/manifests/mainserver/reverseproxy.pp
@@ -3,20 +3,14 @@ class profile::mainserver::reverseproxy {
     package{ 'nginx' :
         ensure => installed,
     }
+  
 
-    service{ 'nginx' :
-        ensure => running, 
-    }
-
-    exec {'include proxy' : 
-        command => 'setsebool httpd_can_network_connect on',
-    }
-    
-    
     file { '/etc/nginx/conf.d/reverseproxy.conf': 
         ensure => file,
         source => 'puppet:///modules/apache/reverseproxy.conf'
     }
 
-    
+    service{ 'nginx' :
+        ensure => running, 
+    }
 }
