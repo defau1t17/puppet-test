ccfe77c1b0a30eccfb009277b257b45d659c8791
defau1t17 <mihail.stepushin@gmail.com>
2024-01-21 02:26:49 +0300

    fix 4.3

--- modules/minecraftserver/manifests/init.pp
+++ modules/minecraftserver/manifests/init.pp
@@ -10,7 +10,7 @@ class minecraftserver {
     include wget
     wget::fetch {'test fetch' :
         source      => 'https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar',
-        destination => '/opt/minecraft/',
+        destination => '/opt/minecraft/minecraft_server.1.20.4.jar',
         timeout     => 0,
         verbose     => false,
     }
@@ -20,7 +20,6 @@ class minecraftserver {
         mode => '0777',
     }
     
-
     file{ '/etc/systemd/system/starter.service' :
         ensure => 'file',
         source => 'puppet:///modules/minecraftserver/starter.service',
@@ -37,6 +36,5 @@ class minecraftserver {
         ensure => running,
         enable => true,
     }
-  
     
 }
