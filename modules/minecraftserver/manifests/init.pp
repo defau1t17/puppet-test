1be460575931540be05f6d0bb84145ecd5094d2b
defau1t17 <mihail.stepushin@gmail.com>
2024-01-21 02:01:12 +0300

    fix 4.0

--- modules/minecraftserver/manifests/init.pp
+++ modules/minecraftserver/manifests/init.pp
@@ -12,6 +12,15 @@ class minecraftserver {
         creates => '/opt/minecraft/server.jar',
     }
 
+    include wget
+    wget::fetch {'test fetch' :
+        source      => 'https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar',
+        destination => '/tmp/',
+        timeout     => 0,
+        verbose     => false,
+    }
+
+
     file{ '/etc/systemd/system/starter.service' :
         ensure => 'file',
         source => 'puppet:///modules/minecraftserver/starter.service',
@@ -21,6 +30,13 @@ class minecraftserver {
         ensure => running,
         enable => true,
     }
+
+    file_line { 'Update Policy':
+      ensure => present,
+      path   => '/opt/minecraft/eula.txt',
+      line   => "eula=true",
+      match  => 'eula=false',
+   }
   
     
 }
