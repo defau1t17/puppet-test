4d5b1eea06cd907fb02cbfad29bd8e751c9ce24e
defau1t17 <mihail.stepushin@gmail.com>
2024-01-20 21:59:59 +0300

    fix

--- modules/minecraftserver/manifests/init.pp
+++ modules/minecraftserver/manifests/init.pp
@@ -11,6 +11,11 @@ class minecraftserver {
         ensure => 'directory',
     }
 
+    exec { 'download minecraft server' :
+        command => '/usr/bin/wget https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar -O /opt/minecraft/server.jar',
+        creates => '/opt/minecraft/server.jar',
+    }
+
     file{ '/etc/systemd/system/starter.service' :
         ensure => 'file',
         source => 'puppet:///modeles/minecraftserver/starter.service',
