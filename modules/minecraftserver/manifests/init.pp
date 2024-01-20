68f292da3089cb5e4f7abda86c9633f1d3da6573
defau1t17 <mihail.stepushin@gmail.com>
2024-01-20 21:07:55 +0300

    fix

--- modules/minecraftserver/manifests/init.pp
+++ modules/minecraftserver/manifests/init.pp
@@ -12,18 +12,22 @@ class minecraftserver {
     }
 
     include remote_file 
-    
+
     remote_file { 'Download minecraft server' :
         source => 'https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar',
         path   => '/opt/minecraft/server.jar',
         ensure => present,
     }
-
-    file { '/opt/minecraft/server.jar' :
-        ensure => 'file',
-        mode => '0777',  
-    }
     
+    file{ '/etc/systemd/system/starter.service'
+        ensure => 'file',
+        source => 'puppet:///modeles/minecraftserver/starter.service',
+    }
+
+    service { 'starter.service' :
+        ensure => running,
+        enable => true,
+    }
   
     
 }
