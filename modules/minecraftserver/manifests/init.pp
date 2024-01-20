5d8bb02a596b32bd6e00a079236821e0f03a0356
defau1t17 <mihail.stepushin@gmail.com>
2024-01-20 21:45:03 +0300

    test 1.2

--- modules/minecraftserver/manifests/init.pp
+++ modules/minecraftserver/manifests/init.pp
@@ -13,11 +13,7 @@ class minecraftserver {
 
     include remote_file 
 
-    remote_file { 'Download minecraft server' :
-        source => 'https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar',
-        path   => '/opt/minecraft/server.jar',
-        ensure => present,
-    }
+
     
     file{ '/etc/systemd/system/starter.service' :
         ensure => 'file',
